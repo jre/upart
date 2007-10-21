2729fccf5835cf07ee2e817dd9eb9f408eb9a029
joshe <joshe@77ae0370-36ea-0310-b45f-a7c9dfe867ce>
2007-10-21 20:43:10 +0000

    Rework getbuf macros and add matching setbuf macros. Add a regression test for getbuf and setbuf macros.

--- util.h
+++ util.h
@@ -1,49 +1,111 @@
 #ifndef HDR_UPART_UTIL
 #define HDR_UPART_UTIL
 
-/* read a 16, 32, or 64 bit little-endian integer from a buffer */
+/* read a 16, 32, or 64 bit little-endian unsigned integer from a buffer */
 
 #define UP_GETBUF16LE(buf) \
-    ((uint16_t)(((const uint8_t *)(buf))[1] <<  8) | \
-     (uint16_t)(((const uint8_t *)(buf))[0]))
+    (((uint16_t)(((const uint8_t *)(buf))[1]) <<  8) | \
+     ((uint16_t)(((const uint8_t *)(buf))[0])))
 
 #define UP_GETBUF32LE(buf) \
-    ((uint32_t)(((const uint8_t *)(buf))[3] << 24) | \
-     (uint32_t)(((const uint8_t *)(buf))[2] << 16) | \
-     (uint32_t)(((const uint8_t *)(buf))[1] <<  8) | \
-     (uint32_t)(((const uint8_t *)(buf))[0]))
+    (((uint32_t)(((const uint8_t *)(buf))[3]) << 24) | \
+     ((uint32_t)(((const uint8_t *)(buf))[2]) << 16) | \
+     ((uint32_t)(((const uint8_t *)(buf))[1]) <<  8) | \
+     ((uint32_t)(((const uint8_t *)(buf))[0])))
 
 #define UP_GETBUF64LE(buf) \
-    ((uint64_t)(((const uint8_t *)(buf))[7] << 56) | \
-     (uint64_t)(((const uint8_t *)(buf))[6] << 48) | \
-     (uint64_t)(((const uint8_t *)(buf))[5] << 40) | \
-     (uint64_t)(((const uint8_t *)(buf))[4] << 32) | \
-     (uint64_t)(((const uint8_t *)(buf))[3] << 24) | \
-     (uint64_t)(((const uint8_t *)(buf))[2] << 16) | \
-     (uint64_t)(((const uint8_t *)(buf))[1] <<  8) | \
-     (uint64_t)(((const uint8_t *)(buf))[0]))
+    (((uint64_t)(((const uint8_t *)(buf))[7]) << 56) | \
+     ((uint64_t)(((const uint8_t *)(buf))[6]) << 48) | \
+     ((uint64_t)(((const uint8_t *)(buf))[5]) << 40) | \
+     ((uint64_t)(((const uint8_t *)(buf))[4]) << 32) | \
+     ((uint64_t)(((const uint8_t *)(buf))[3]) << 24) | \
+     ((uint64_t)(((const uint8_t *)(buf))[2]) << 16) | \
+     ((uint64_t)(((const uint8_t *)(buf))[1]) <<  8) | \
+     ((uint64_t)(((const uint8_t *)(buf))[0])))
 
-/* read a 16, 32, or 64 bit big-endian integer from a buffer */
+/* read a 16, 32, or 64 bit big-endian unsigned integer from a buffer */
 
 #define UP_GETBUF16BE(buf) \
-    ((uint16_t)(((const uint8_t *)(buf))[0] <<  8) | \
-     (uint16_t)(((const uint8_t *)(buf))[1]))
+    (((uint16_t)(((const uint8_t *)(buf))[0]) <<  8) | \
+     ((uint16_t)(((const uint8_t *)(buf))[1])))
 
 #define UP_GETBUF32BE(buf) \
-    ((uint32_t)(((const uint8_t *)(buf))[0] << 24) | \
-     (uint32_t)(((const uint8_t *)(buf))[1] << 16) | \
-     (uint32_t)(((const uint8_t *)(buf))[2] <<  8) | \
-     (uint32_t)(((const uint8_t *)(buf))[3]))
+    (((uint32_t)(((const uint8_t *)(buf))[0]) << 24) | \
+     ((uint32_t)(((const uint8_t *)(buf))[1]) << 16) | \
+     ((uint32_t)(((const uint8_t *)(buf))[2]) <<  8) | \
+     ((uint32_t)(((const uint8_t *)(buf))[3])))
 
 #define UP_GETBUF64BE(buf) \
-    ((uint64_t)(((const uint8_t *)(buf))[0] << 56) | \
-     (uint64_t)(((const uint8_t *)(buf))[1] << 48) | \
-     (uint64_t)(((const uint8_t *)(buf))[2] << 40) | \
-     (uint64_t)(((const uint8_t *)(buf))[3] << 32) | \
-     (uint64_t)(((const uint8_t *)(buf))[4] << 24) | \
-     (uint64_t)(((const uint8_t *)(buf))[5] << 16) | \
-     (uint64_t)(((const uint8_t *)(buf))[6] <<  8) | \
-     (uint64_t)(((const uint8_t *)(buf))[7]))
+    (((uint64_t)(((const uint8_t *)(buf))[0]) << 56) | \
+     ((uint64_t)(((const uint8_t *)(buf))[1]) << 48) | \
+     ((uint64_t)(((const uint8_t *)(buf))[2]) << 40) | \
+     ((uint64_t)(((const uint8_t *)(buf))[3]) << 32) | \
+     ((uint64_t)(((const uint8_t *)(buf))[4]) << 24) | \
+     ((uint64_t)(((const uint8_t *)(buf))[5]) << 16) | \
+     ((uint64_t)(((const uint8_t *)(buf))[6]) << 8) | \
+     ((uint64_t)(((const uint8_t *)(buf))[7])))
+
+/* write a 16, 32, or 64 bit little-endian unsigned integer to a buffer */
+
+#define UP_SETBUF16LE(buf, val) \
+    do { \
+        uint8_t *up_setbuf16le_buf = (buf); \
+        up_setbuf16le_buf[1] = ((uint16_t)(val) >>  8) & 0xff; \
+        up_setbuf16le_buf[0] =  (uint16_t)(val)        & 0xff; \
+    } while(0)
+
+#define UP_SETBUF32LE(buf, val) \
+    do { \
+        uint8_t *up_setbuf32le_buf = (buf); \
+        up_setbuf32le_buf[3] = ((uint32_t)(val) >> 24) & 0xff; \
+        up_setbuf32le_buf[2] = ((uint32_t)(val) >> 16) & 0xff; \
+        up_setbuf32le_buf[1] = ((uint32_t)(val) >>  8) & 0xff; \
+        up_setbuf32le_buf[0] =  (uint32_t)(val)        & 0xff; \
+    } while(0)
+
+#define UP_SETBUF64LE(buf, val) \
+    do { \
+        uint8_t *up_setbuf64le_buf = (buf); \
+        up_setbuf64le_buf[7] = ((uint64_t)(val) >> 56) & 0xff; \
+        up_setbuf64le_buf[6] = ((uint64_t)(val) >> 48) & 0xff; \
+        up_setbuf64le_buf[5] = ((uint64_t)(val) >> 40) & 0xff; \
+        up_setbuf64le_buf[4] = ((uint64_t)(val) >> 32) & 0xff; \
+        up_setbuf64le_buf[3] = ((uint64_t)(val) >> 24) & 0xff; \
+        up_setbuf64le_buf[2] = ((uint64_t)(val) >> 16) & 0xff; \
+        up_setbuf64le_buf[1] = ((uint64_t)(val) >>  8) & 0xff; \
+        up_setbuf64le_buf[0] =  (uint64_t)(val)        & 0xff; \
+    } while(0)
+
+/* write a 16, 32, or 64 bit big-endian unsigned integer to a buffer */
+
+#define UP_SETBUF16BE(buf, val) \
+    do { \
+        uint8_t *up_setbuf16be_buf = (buf); \
+        up_setbuf16be_buf[0] = ((uint16_t)(val) >>  8) & 0xff; \
+        up_setbuf16be_buf[1] =  (uint16_t)(val)        & 0xff; \
+    } while(0)
+
+#define UP_SETBUF32BE(buf, val) \
+    do { \
+        uint8_t *up_setbuf32be_buf = (buf); \
+        up_setbuf32be_buf[0] = ((uint32_t)(val) >> 24) & 0xff; \
+        up_setbuf32be_buf[1] = ((uint32_t)(val) >> 16) & 0xff; \
+        up_setbuf32be_buf[2] = ((uint32_t)(val) >>  8) & 0xff; \
+        up_setbuf32be_buf[3] =  (uint32_t)(val)        & 0xff; \
+    } while(0)
+
+#define UP_SETBUF64BE(buf, val) \
+    do { \
+        uint8_t *up_setbuf64be_buf = (buf); \
+        up_setbuf64be_buf[0] = ((uint64_t)(val) >> 56) & 0xff; \
+        up_setbuf64be_buf[1] = ((uint64_t)(val) >> 48) & 0xff; \
+        up_setbuf64be_buf[2] = ((uint64_t)(val) >> 40) & 0xff; \
+        up_setbuf64be_buf[3] = ((uint64_t)(val) >> 32) & 0xff; \
+        up_setbuf64be_buf[4] = ((uint64_t)(val) >> 24) & 0xff; \
+        up_setbuf64be_buf[5] = ((uint64_t)(val) >> 16) & 0xff; \
+        up_setbuf64be_buf[6] = ((uint64_t)(val) >>  8) & 0xff; \
+        up_setbuf64be_buf[7] =  (uint64_t)(val)        & 0xff; \
+    } while(0)
 
 /*
   Dump SIZE bytes of data in BUF to STREAM to stdout, in

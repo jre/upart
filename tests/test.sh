1f3ba31e651b86e061bfe48d10d9af7bc3dd42da
Joshua Elsasser <joshua@elsasser.org>
2010-04-05 00:31:32 -0700

    Run tests at verbosity levels 0 to 2 instead of only at 2.
    Also tweak 'make clean' to look nicer for the tests.

--- tests/test.sh
+++ tests/test.sh
@@ -2,18 +2,21 @@
 
 if [ -n "$UPART_TEST_CLEAN" ]
 then
-    cleanfiles=
-    for ii in *.img
+    for img in *.img
     do
-        test="`basename "$ii" .img`"
-        cleanfiles="$cleanfiles test-${test}.out test-${test}.err"
+        cleanfiles=
+        for flag in '' -v -vv
+        do
+            test="`basename "$img" .img`$flag"
+            cleanfiles="$cleanfiles test-${test}.out test-${test}.err"
+        done
+        echo "rm -f$cleanfiles"
+        rm -f $cleanfiles
     done
-    echo "rm -f$cleanfiles"
-    rm -f $cleanfiles
     exit
 fi
 
-upart="../upart -fvv"
+upart="../upart -f"
 diff="diff -u"
 
 failcount=0
@@ -22,86 +25,92 @@ faillist=
 
 if [ -n "$UPART_TEST_REGEN" ]
 then
-    for ii in *.img
+    for img in *.img
     do
-        test="`basename "$ii" .img`"
-        testoutfile="$test.out"
-        testerrfile="$test.err"
-        testexitfile="$test.exit"
-        rm -f "$testoutfile" "$testerrfile" "$testexitfile"
-        $upart "$ii" > "$testoutfile" 2> "$testerrfile"
-        exit=$?
-        test -s "$testoutfile" || rm -f "$testoutfile"
-        test -s "$testerrfile" || rm -f "$testerrfile"
-        test 0 = $exit || echo $exit > "$testexitfile"
+        for flag in '' -v -vv
+        do
+            test="`basename "$img" .img`$flag"
+            testoutfile="$test.out"
+            testerrfile="$test.err"
+            testexitfile="$test.exit"
+            rm -f "$testoutfile" "$testerrfile" "$testexitfile"
+            $upart $flag "$img" > "$testoutfile" 2> "$testerrfile"
+            exit=$?
+            test -s "$testoutfile" || rm -f "$testoutfile"
+            test -s "$testerrfile" || rm -f "$testerrfile"
+            test 0 = $exit || echo $exit > "$testexitfile"
+        done
     done
     exit
 fi
 
 echo "running tests..."
 
-for ii in *.img
+for img in *.img
 do
-    test="`basename "$ii" .img`"
-    testoutfile="test-$test.out"
-    testerrfile="test-$test.err"
+    for flag in '' -v -vv
+    do
+        test="`basename "$img" .img`$flag"
+        testoutfile="test-$test.out"
+        testerrfile="test-$test.err"
 
-    if [ -r "$test.exit" ]
-    then
-        wantstatus="`cat "$test.exit"`"
-    else
-        wantstatus=0
-    fi
+        if [ -r "$test.exit" ]
+        then
+            wantstatus="`cat "$test.exit"`"
+        else
+            wantstatus=0
+        fi
 
-    if [ ! -r "$test.out" ]
-    then
-        outfile=/dev/null
-    else
-        outfile="$test.out"
-    fi
+        if [ ! -r "$test.out" ]
+        then
+            outfile=/dev/null
+        else
+            outfile="$test.out"
+        fi
 
-    if [ ! -r "$test.err" ]
-    then
-        errfile=/dev/null
-    else
-        errfile="$test.err"
-    fi
+        if [ ! -r "$test.err" ]
+        then
+            errfile=/dev/null
+        else
+            errfile="$test.err"
+        fi
 
-    rm -f "$testoutfile" "$testerrfile"
-    failed=n
-    $upart "$ii" > "$testoutfile" 2> "$testerrfile"
-    gotstatus=$?
+        rm -f "$testoutfile" "$testerrfile"
+        failed=n
+        $upart $flag "$img" > "$testoutfile" 2> "$testerrfile"
+        gotstatus=$?
 
-    if [ x"$wantstatus" != x"$gotstatus" ]
-    then
-        failed=y
-        echo "$test: expected exit status $wantstatus $gotstatus"
-    fi
+        if [ x"$wantstatus" != x"$gotstatus" ]
+        then
+            failed=y
+            echo "$test: expected exit status $wantstatus $gotstatus"
+        fi
 
-    if cmp "$outfile" "$testoutfile" >/dev/null 2>&1
-    then
-        rm -f "$testoutfile"
-    else
-        failed=y
-        echo "$test: unexpected standard output"
-        $diff "$outfile" "$testoutfile"
-    fi
+        if cmp "$outfile" "$testoutfile" >/dev/null 2>&1
+        then
+            rm -f "$testoutfile"
+        else
+            failed=y
+            echo "$test: unexpected standard output"
+            $diff "$outfile" "$testoutfile"
+        fi
 
-    if cmp "$errfile" "$testerrfile" >/dev/null 2>&1
-    then
-        rm -f "$testerrfile"
-    else
-        failed=y
-        echo "$test: unexpected standard error"
-        $diff "$errfile" "$testerrfile"
-    fi
+        if cmp "$errfile" "$testerrfile" >/dev/null 2>&1
+        then
+            rm -f "$testerrfile"
+        else
+            failed=y
+            echo "$test: unexpected standard error"
+            $diff "$errfile" "$testerrfile"
+        fi
 
-    totalcount="`expr "$totalcount" \+ 1`"
-    if [ xy = x"$failed" ]
-    then
-        failcount="`expr "$failcount" \+ 1`"
-        faillist="$faillist $test"
-    fi
+        totalcount="`expr "$totalcount" \+ 1`"
+        if [ xy = x"$failed" ]
+        then
+            failcount="`expr "$failcount" \+ 1`"
+            faillist="$faillist $test"
+        fi
+    done
 done
 
 if [ 1 = $failcount ]

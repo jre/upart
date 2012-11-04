3c11ad012ca049a3ee40d58ddab4b1b26b3296e6
Joshua Elsasser <joshua@elsasser.org>
2012-11-03 22:54:47 -0700

    Import md5 code.

--- defs.mk
+++ defs.mk
@@ -6,7 +6,7 @@ ALL_SRCS      = $(UPART_SRCS)
 ALL_HDRS      = $(UPART_HDRSRC:.=.h) bsdqueue.h os-private.h
 UPART_SRCS    = $(UPART_HDRSRC:.=.c) getopt.c main.c os-bsd.c os-darwin.c \
 		os-haiku.c os-linux.c os-solaris.c os-unix.c os-windows.c
-UPART_HDRSRC  = apm. bsdlabel. crc32. disk. gpt. img. map. mbr. os. \
+UPART_HDRSRC  = apm. bsdlabel. crc32. disk. gpt. img. map. md5. mbr. os. \
 		sunlabel-shared. sunlabel-sparc. sunlabel-x86. util.
 REGRESS_SRC   = tests/tester.c
 REGRESS_BIN   = tests/tester
